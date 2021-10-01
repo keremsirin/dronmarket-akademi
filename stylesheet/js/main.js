ced3d5e720e133941349a63642d238b432e973c2
Kerem Şirin <30510378+keremsirin@users.noreply.github.com>
2021-10-01 15:36:54 +0300

    glider 2. carousel eklendi

--- stylesheet/js/main.js
+++ stylesheet/js/main.js
@@ -12,8 +12,10 @@ document.getElementById('hamburger-menu').onclick = menuToggle
 
 function removeGlider() {
   var x = document.getElementsByClassName('glider-remove')[0]
+  var y = document.getElementsByClassName('glider-remove')[1]
   if (window.innerWidth > 992) {
     x.classList.remove('glider')
+    y.classList.remove('glider2')
   } else {
     // window.addEventListener('resize', function () {
     //   x.classList.add('glider')
@@ -31,6 +33,14 @@ function removeGlider() {
         slidesToShow: 1,
         draggable: true,
         dots: '.dots',
+        scrollLock: true
+      })
+      y.classList.add('glider2')
+      new Glider(document.querySelector('.glider2'), {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        draggable: true,
+        dots: '.dots2',
         scrollLock: true
       })
     })
